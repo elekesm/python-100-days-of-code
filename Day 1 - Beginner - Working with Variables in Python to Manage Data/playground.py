7c7897b73afa2482503dcff4836c4cc9015b82f6
elekesm <elekes.mark@gmail.com>
2026-05-26 23:02:36 +0200

    documentation

--- Day 1 - Beginner - Working with Variables in Python to Manage Data/playground.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/playground.py
@@ -1,4 +1,23 @@
-# This script demonstrates basic Python print statements and string concatenation.
+"""
+This script demonstrates basic Python concepts including print statements, string concatenation, 
+user input, and variable usage. It is structured as follows:
+
+1. Basic Print Statements:
+    - Outputs simple messages to the console.
+    - Demonstrates the use of newline characters (`\n`) and string concatenation.
+
+2. User Input:
+    - Prompts the user for their name and personalizes the output message.
+
+3. Variables:
+    - Introduces the concept of variables to store and manipulate data.
+    - Demonstrates how to calculate and display the length of a user-provided name.
+
+Usage:
+- Run the script and follow the prompts to see personalized messages and interact with the program.
+"""
+
+
 print("Hello Python!")
 print("Hello! \nI's Mark")
 print("Hello" + " " + "World" + "!" )
--- Day 1 - Beginner - Working with Variables in Python to Manage Data/task.py
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/task.py
@@ -1,3 +1,11 @@
+"""
+This script generates a band name based on user input.
+
+The program prompts the user to input the name of the city they grew up in
+and the name of their pet. It then combines these inputs to create and display
+a potential band name.
+"""
+
 print("Welcome the bandname generator!")
 
 city = input("What's the name of the city you grew up in?\n")
