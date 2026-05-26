9527851a616e49df6e353007a450772cc199a83f
elekesm <elekes.mark@gmail.com>
2026-05-26 22:58:05 +0200

    first day practice

---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/playground.py
@@ -0,0 +1,15 @@
+# This script demonstrates basic Python print statements and string concatenation.
+print("Hello Python!")
+print("Hello! \nI's Mark")
+print("Hello" + " " + "World" + "!" )
+
+# Includes user input to personalize the output.
+print("Hello, i's Mark\n" + "Nice to meet you " + input("What's your name?\n"))
+
+# This section demonstrates how to use variables to store and manipulate data.
+print("------variables------")
+name = "Mark"
+print(name)
+
+# How to calculate the length of a name
+print("The lenght of the name is: " + str(len(input("Add a name to calculate the length of it\n"))) + " char long")
---
+++ Day 1 - Beginner - Working with Variables in Python to Manage Data/task.py
@@ -0,0 +1,5 @@
+print("Welcome the bandname generator!")
+
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print("Your band name could be " + city + " " + pet)
