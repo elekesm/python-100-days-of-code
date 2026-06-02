911afeb5f853274a9457032686f5fec23c6901a9
elekesm <elekes.mark@gmail.com>
2026-06-02 20:24:44 +0200

    day6 loops

---
+++ Day 6 - functions/01_while_loop.py
@@ -0,0 +1,22 @@
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
+
+def my_second_function():
+    print("Hello")
+    print("Bye")
+
+cake_sclices = 6
+while cake_sclices > 0:
+    print("I am eating a slice of cake WHILE there are still slices left")
+    cake_sclices -= 1
+print("No more cake left :(")
+
+
+for slice in range(3):
+    print("I am eating a slice of cake FOR my pleasure")
+print("No more cake left :(")
+
+# This day is primarily dedicated to practicing writing functions on Reeborg.ca.
